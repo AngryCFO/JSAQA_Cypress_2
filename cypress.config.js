70b6a10cc093fe1e9c066e27792d76df939dbb29
AngryCFO <147319756+AngryCFO@users.noreply.github.com>
2024-11-20 23:20:58 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "tfwkz8",
+  projectId: "4amvbm",
   e2e: {
     baseUrl: "https://petstore.swagger.io/v2/",
     //retries: 2,
